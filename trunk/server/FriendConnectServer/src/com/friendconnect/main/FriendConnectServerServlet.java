b70c022cc00d93aa703153e5d04397614b306d16
juri.strumpflohner <juri.strumpflohner@59419c26-b896-11de-a210-8fd6ca7157a7>
2009-10-20 10:20:11 +0000

    1st rpc call worked!!!

--- trunk/server/FriendConnectServer/src/com/friendconnect/main/FriendConnectServerServlet.java
+++ trunk/server/FriendConnectServer/src/com/friendconnect/main/FriendConnectServerServlet.java
@@ -3,7 +3,11 @@ package com.friendconnect.main;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.xmlrpc.webserver.WebServer;
 
 @SuppressWarnings("serial")
 public class FriendConnectServerServlet extends HttpServlet {
@@ -11,6 +15,11 @@ public class FriendConnectServerServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		super.init();
+		
+		//initiate the XML-RPC server
+		WebServer server = new WebServer(8080);
+		
+		
 	}
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
