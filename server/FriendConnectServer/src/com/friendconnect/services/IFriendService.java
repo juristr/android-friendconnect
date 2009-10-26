1458c55af6f0b8b03fce86c373f3f5956594b409
Juri Strumpflohner <info@juristr.com>
2009-10-26 10:08:33 +0000

    FriendService

---
+++ server/FriendConnectServer/src/com/friendconnect/services/IFriendService.java
@@ -0,0 +1,27 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2009, FriendConnect			                       				 **
+ **   All rights reserved.                                                   **
+ **                                                                          **
+ **	  This program and the accompanying materials are made available under   **
+ **   the terms of the GPLv3 license which accompanies this	    			 **
+ **   distribution. A copy is found in the textfile LICENSE.txt				 **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Main developers:                                                       **
+ **     Juri Strumpflohner		http://blog.js-development.com	             **
+ **     Matthias Braunhofer		http://matthias.jimdo.com	                 **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package com.friendconnect.services;
+
+import java.util.List;
+
+import com.friendconnect.model.Friend;
+
+public interface IFriendService {
+	List<Friend> getFriends();
+}
--- server/FriendConnectServer/src/com/friendconnect/xmlrpc/XMLRPCGateway.java
+++ server/FriendConnectServer/src/com/friendconnect/xmlrpc/XMLRPCGateway.java
@@ -2,23 +2,41 @@ package com.friendconnect.xmlrpc;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.friendconnect.model.Friend;
 import com.friendconnect.model.TestDTO;
+import com.friendconnect.services.AuthenticationService;
+import com.friendconnect.services.FriendService;
+import com.friendconnect.services.IAuthenticationService;
+import com.friendconnect.services.IFriendService;
 
 public class XMLRPCGateway {
-
+	private IAuthenticationService authService; //TODO inject this
+	private IFriendService friendService; //TODO inject this
+	
 	public XMLRPCGateway() {
-
+		//TODO these initializations will be injected!!!
+		friendService = new FriendService();
+		authService = new AuthenticationService();		
 	}
 
 	public String getFirstname() {
 		return "test";
 	}
-
-	public String answer(String incoming) {
-		return incoming + " - Hi there!";
+	
+	public void getFriends(String username, String token){
+		boolean isAuthenticated = authService.validateToken(username, token);
+		if(isAuthenticated){
+			List<Friend> friends = friendService.getFriends();
+			
+			
+		}else{
+			//TODO handle authentication failure appropriately, once for all method calls
+		}
 	}
+	
 
 	public Map<String, Object> modifyObject(Map<String, Object> person) throws IllegalArgumentException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		ObjectDeserializer<TestDTO> deserializer = new ObjectDeserializer<TestDTO>(
