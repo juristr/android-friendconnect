1187eb2c7ffa909434493cf4296c9e85245f6fe3
Juri Strumpflohner <info@juristr.com>
2009-10-26 09:01:57 +0000

    xml-rpc tests

---
+++ server/FriendConnectServer/src/com/friendconnect/model/TestDTO.java
@@ -0,0 +1,61 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2009, FriendConnect			                       				 **
+ **   All rights reserved.                                                   **
+ **                                                                          **
+ **	  This program and the accompanying materials are made available under   **
+ **   the terms of the GPLv3 license which accompanies this	    			 **
+ **   distribution. A copy is found in the textfile LICENSE.txt				 **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Main developers:                                                       **
+ **     Juri Strumpflohner		http://blog.js-development.com	             **
+ **     Matthias Braunhofer		http://matthias.jimdo.com	                 **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package com.friendconnect.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestDTO implements Serializable {
+	private String firstname;
+	private String lastname;
+
+	public TestDTO(){
+		
+	}
+	
+	public TestDTO(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Object getSerializable() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("firstname", this.firstname);
+		map.put("lastname", this.lastname);
+		return map;
+	}
+
+}
