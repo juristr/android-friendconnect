59843435de69b38f1ec42c6a20408428930d5de9
juri.strumpflohner <juri.strumpflohner@59419c26-b896-11de-a210-8fd6ca7157a7>
2009-10-29 10:20:27 +0000

    started to implement FriendUpdateService

---
+++ trunk/client/FriendConnect/src/com/friendconnect/main/FriendConnectApplication.java
@@ -0,0 +1,43 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2009, FriendConnect			                       				 **
+ **   All rights reserved.                                                   **
+ **                                                                          **
+ **	  This program and the accompanying materials are made available under   **
+ **   the terms of the GPLv3 license which accompanies this	    			 **
+ **   distribution. A copy is found in the textfile LICENSE.txt				 **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Main developers:                                                       **
+ **     Juri Strumpflohner		http://blog.js-development.com	             **
+ **     Matthias Braunhofer		http://matthias.jimdo.com	                 **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package com.friendconnect.main;
+
+import android.app.Application;
+
+public class FriendConnectApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+	
+	@Override
+	public void onTerminate() {
+		// TODO Auto-generated method stub
+		super.onTerminate();
+	}
+	
+	@Override
+	public void onLowMemory() {
+		// TODO Auto-generated method stub
+		super.onLowMemory();
+	}
+	
+}
