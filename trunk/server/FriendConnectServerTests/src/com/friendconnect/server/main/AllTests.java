ca1ac2348711accfb27164994eac5205c6d50dac
juri.strumpflohner <juri.strumpflohner@59419c26-b896-11de-a210-8fd6ca7157a7>
2009-10-31 09:33:49 +0000

    added test suite for all tests (except authentication)

---
+++ trunk/server/FriendConnectServerTests/src/com/friendconnect/server/main/AllTests.java
@@ -0,0 +1,37 @@
+/*   **********************************************************************  **
+ **   Copyright notice                                                       **
+ **                                                                          **
+ **   (c) 2009, FriendConnect			                       				 **
+ **   All rights reserved.                                                   **
+ **                                                                          **
+ **	  This program and the accompanying materials are made available under   **
+ **   the terms of the GPLv3 license which accompanies this	    			 **
+ **   distribution. A copy is found in the textfile LICENSE.txt				 **
+ **                                                                          **
+ **   This copyright notice MUST APPEAR in all copies of the file!           **
+ **                                                                          **
+ **   Main developers:                                                       **
+ **     Juri Strumpflohner		http://blog.js-development.com	             **
+ **     Matthias Braunhofer		http://matthias.jimdo.com	                 **
+ **                                                                          **
+ **  **********************************************************************  */
+
+package com.friendconnect.server.main;
+
+import com.friendconnect.server.tests.xmlrpc.ObjectSerializerTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for com.friendconnect.server");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(ObjectSerializerTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
