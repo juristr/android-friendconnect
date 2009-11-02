3c8d6a19bd4e0e3a0397dd5b2182cb04b26d4d30
matthias.braunhofer <matthias.braunhofer@59419c26-b896-11de-a210-8fd6ca7157a7>
2009-11-02 17:38:12 +0000

    Added comments

--- trunk/server/FriendConnectServer/src/com/friendconnect/dao/IUserDao.java
+++ trunk/server/FriendConnectServer/src/com/friendconnect/dao/IUserDao.java
@@ -22,15 +22,43 @@ import java.util.List;
 
 import com.friendconnect.model.User;
 
+/**
+ * Interface of a DAO object to store and retrieve user data into/from a JDO Datastore
+ */
 public interface IUserDao {
 	
+	/**
+	 * Saves a user into the JDO Datastore
+	 * @param user
+	 */
 	public void saveUser(User user);
 	
+	/**
+	 * Fetches a user from the JDO Datastore passing its id
+	 * @param userId
+	 * @param loadFriends
+	 * @return
+	 */
 	public User getUserById(String userId, boolean loadFriends);
 	
+	/**
+	 * Fetches a user from the JDO Datastore passing its email address
+	 * @param emailAddress
+	 * @param loadFriends
+	 * @return
+	 */
 	public User getUserByEmailAddress(String emailAddress, boolean loadFriends);
 	
+	/**
+	 * Deletes a user from the JDO Datastore
+	 * @param userId
+	 */
 	public void removeUser(String userId);
 	
+	/**
+	 * Returns all users from the JDO Datastore that match the specified search criteria
+	 * @param searchText
+	 * @return
+	 */
 	public List<User> searchUsers(String searchText);
 }
