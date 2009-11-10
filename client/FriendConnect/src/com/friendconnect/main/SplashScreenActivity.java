67b8306c9d1f8409d88881bcf122a1178f985f92
matthias.braunhofer <mbraunhofer@users.noreply.github.com>
2009-11-10 11:25:09 +0000

    Finished SplashScreenActivity

--- client/FriendConnect/src/com/friendconnect/main/SplashScreenActivity.java
+++ client/FriendConnect/src/com/friendconnect/main/SplashScreenActivity.java
@@ -18,6 +18,63 @@
 
 package com.friendconnect.main;
 
-public class SplashScreenActivity {
+import com.friendconnect.R;
+import com.friendconnect.activities.FriendListActivity;
+import com.friendconnect.activities.LoginActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashScreenActivity extends Activity {
+	private long splashTime = 3000;
+	private boolean paused = false;
+	private boolean splashActive = true;
+
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+
+		// Draw the splash screen
+		setContentView(R.layout.splash);
+		// Very simple timer thread
+		Thread splashTimer = new Thread() {
+			public void run() {
+				try {
+					// Wait loop
+					long ms = 0;
+					while (splashActive && ms < splashTime) {
+						sleep(100);
+						// Advance the timer only if we're running.
+						if (!paused)
+							ms += 100;
+					}
+					// Advance to the correct next screen
+					FriendConnectApplication application = IoC.getInstance(FriendConnectApplication.class);
+					Intent intent = null;
+					if (application.getApplicationModel() == null) {
+						intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+					} else {
+						intent = new Intent(SplashScreenActivity.this, FriendListActivity.class);
+					}
+					startActivity(intent);
+				} catch (Exception e) {
+					Log.e("Splash", e.toString());
+				} finally {
+					finish();
+				}
+			}
+		};
+		splashTimer.start();
+	}
+
+	protected void onPause() {
+		super.onPause();
+		paused = true;
+	}
+
+	protected void onResume() {
+		super.onResume();
+		paused = false;
+	}
 }
