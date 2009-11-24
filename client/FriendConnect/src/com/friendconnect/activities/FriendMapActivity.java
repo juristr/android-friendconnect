177da94a6c040ffefdfe213a1e0824242a0dbb66
Juri Strumpflohner <info@juristr.com>
2009-11-24 08:11:17 +0000

    removed dummy loc set dialog

--- client/FriendConnect/src/com/friendconnect/activities/FriendMapActivity.java
+++ client/FriendConnect/src/com/friendconnect/activities/FriendMapActivity.java
@@ -22,26 +22,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Observable;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.friendconnect.R;
 import com.friendconnect.controller.LocationController;
 import com.friendconnect.main.IoC;
 import com.friendconnect.model.FriendConnectUser;
-import com.friendconnect.model.Location;
 import com.friendconnect.model.User;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -51,9 +44,7 @@ import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 
 public class FriendMapActivity extends MapActivity implements IView {
-	private static final int DUMMY_LOCSET = Menu.FIRST;
 
-	static final private int DUMMY_LOCATIONSETTERDIALOG = 10;
 
 	private MapView mapView;
 	private MapController mapController;
@@ -115,8 +106,8 @@ public class FriendMapActivity extends MapActivity implements IView {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		// Create and add new menu items.
-		MenuItem itemAddFriend = menu.add(0, DUMMY_LOCSET, Menu.NONE,
-				"dummy loc");
+//		MenuItem itemAddFriend = menu.add(0, DUMMY_LOCSET, Menu.NONE,
+//				"dummy loc");
 
 		return true;
 	}
@@ -125,67 +116,67 @@ public class FriendMapActivity extends MapActivity implements IView {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		super.onOptionsItemSelected(item);
 		// int index = this.listViewFriends.getSelectedItemPosition();
-		switch (item.getItemId()) {
-		case (DUMMY_LOCSET): {
-			showDialog(DUMMY_LOCATIONSETTERDIALOG);
-			return true;
-		}
-		}
+//		switch (item.getItemId()) {
+//		case (DUMMY_LOCSET): {
+//			showDialog(DUMMY_LOCATIONSETTERDIALOG);
+//			return true;
+//		}
+//		}
 		return false;
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
-		switch (id) {
-		case (DUMMY_LOCATIONSETTERDIALOG): {
-			LayoutInflater li = LayoutInflater.from(this);
-			View dummyLocationView = li.inflate(
-					R.layout.dummylocationsetdialog, null);
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			AlertDialog dummyLocationDialog = builder.create();
-			dummyLocationDialog.setTitle("Dummy Location setter");
-			dummyLocationDialog.setView(dummyLocationView);
-			dummyLocationDialog.setCanceledOnTouchOutside(true);
-			dummyLocationDialog.setIcon(R.drawable.icon);
-			return dummyLocationDialog;
-		}
-		}
+//		switch (id) {
+//		case (DUMMY_LOCATIONSETTERDIALOG): {
+//			LayoutInflater li = LayoutInflater.from(this);
+//			View dummyLocationView = li.inflate(
+//					R.layout.dummylocationsetdialog, null);
+//			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//			AlertDialog dummyLocationDialog = builder.create();
+//			dummyLocationDialog.setTitle("Dummy Location setter");
+//			dummyLocationDialog.setView(dummyLocationView);
+//			dummyLocationDialog.setCanceledOnTouchOutside(true);
+//			dummyLocationDialog.setIcon(R.drawable.icon);
+//			return dummyLocationDialog;
+//		}
+//		}
 		return null;
 	}
 
 	@Override
 	protected void onPrepareDialog(int id, final Dialog dialog) {
-		switch (id) {
-		case (DUMMY_LOCATIONSETTERDIALOG): {
-			((Button) dialog.findViewById(R.id.buttonSubmitLoc))
-					.setOnClickListener(new OnClickListener() {
-						public void onClick(View v) {
-							EditText editTextLat = (EditText) dialog
-									.findViewById(R.id.editTextLatitude);
-							EditText editTextLng = (EditText) dialog
-									.findViewById(R.id.editTextLongitude);
-
-							double lat = Double.parseDouble(editTextLat
-									.getText().toString());
-							double lng = Double.parseDouble(editTextLng
-									.getText().toString());
-
-							final Location loc = new Location();
-							loc.setLatitude(lat);
-							loc.setLongitude(lng);
-
-							Handler handler = new Handler();
-							handler.post(new Runnable() {
-								public void run() {
-									controller.updateDummyLocation(loc);
-								}
-							});
-
-							dialog.dismiss();
-						}
-					});
-		}
-		}
+//		switch (id) {
+//		case (DUMMY_LOCATIONSETTERDIALOG): {
+//			((Button) dialog.findViewById(R.id.buttonSubmitLoc))
+//					.setOnClickListener(new OnClickListener() {
+//						public void onClick(View v) {
+//							EditText editTextLat = (EditText) dialog
+//									.findViewById(R.id.editTextLatitude);
+//							EditText editTextLng = (EditText) dialog
+//									.findViewById(R.id.editTextLongitude);
+//
+//							double lat = Double.parseDouble(editTextLat
+//									.getText().toString());
+//							double lng = Double.parseDouble(editTextLng
+//									.getText().toString());
+//
+//							final Location loc = new Location();
+//							loc.setLatitude(lat);
+//							loc.setLongitude(lng);
+//
+//							Handler handler = new Handler();
+//							handler.post(new Runnable() {
+//								public void run() {
+//									controller.updateDummyLocation(loc);
+//								}
+//							});
+//
+//							dialog.dismiss();
+//						}
+//					});
+//		}
+//		}
 	}
 
 	private void addOverlay(Overlay overlay) {
@@ -219,31 +210,30 @@ public class FriendMapActivity extends MapActivity implements IView {
 	public void update(Observable observable, Object data) {
 		final FriendConnectUser user = (FriendConnectUser) observable;
 
-//		// update friend's positions
-//		for (User friend : user.getFriends()) {
-//			FriendPositionOverlay friendOverlay = friendOverlays.get(friend
-//					.getId());
-//			if (friendOverlay != null) {
-//				friendOverlay.setPosition(friend.getPosition(), user
-//						.getPosition());
-//			} else {
-//				friendOverlay = new FriendPositionOverlay(friend.toString(),
-//						this);
-//				friendOverlays.put(friend.getId(), friendOverlay);
-//				addOverlay(friendOverlay);
-//			}
-//		}
-//
-//		// remove overlays of past friends
-//		for (String friendId : friendOverlays.keySet()) {
-//			if (!containsFriend(friendId, user.getFriends())) {
-//				friendOverlays.remove(friendId);
-//				mapView.getOverlays().remove(friendOverlays.get(friendId));
-//			}
-//		}
+		// update friend's positions
+		for (User friend : user.getFriends()) {
+			FriendPositionOverlay friendOverlay = friendOverlays.get(friend
+					.getId());
+			if (friendOverlay != null) {
+				friendOverlay.setPosition(friend.getPosition(), user
+						.getPosition());
+			} else {
+				friendOverlay = new FriendPositionOverlay(friend.toString(),
+						this);
+				friendOverlays.put(friend.getId(), friendOverlay);
+				addOverlay(friendOverlay);
+			}
+		}
+
+		// remove overlays of past friends
+		for (String friendId : friendOverlays.keySet()) {
+			if (!containsFriend(friendId, user.getFriends())) {
+				friendOverlays.remove(friendId);
+				mapView.getOverlays().remove(friendOverlays.get(friendId));
+			}
+		}
 
 		mapView.invalidate();
-		
 		navigateToPoint(user.getPosition().getLatitude(), user.getPosition()
 				.getLongitude());
 	}
